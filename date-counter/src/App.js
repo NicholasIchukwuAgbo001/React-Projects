2cbdcef219c615a4f55054ffa5e192e88fee9e9e
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-06-27 16:59:49 +0100

    ok

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -12,6 +12,7 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   const date = new Date("June 21, 2027");
   date.setDate(date.getDate() + count);
@@ -19,14 +20,26 @@ function Counter() {
   return (
     <div>
       <div>
-        <button onClick={()=> setCount((counter) => counter - 1)}>-</button>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
         <span>Count: {count}</span>
-        <button onClick={()=> setCount((counter) => counter + 1)}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
 
       <p>
-        <span>{count === 0? "Today is, " : ""}</span>
-        <span>{date.toLocaleDateString()}</span>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
       </p>
     </div>
   );
