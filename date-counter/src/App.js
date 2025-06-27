26a6ad19eeb00ec640a663ff5b91daaf2fe0a9d4
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-06-27 16:50:56 +0100

    commit

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -20,27 +20,10 @@ function Counter() {
   return (
     <div>
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
-      </div>
-
-      <div>
-        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <button onClick={()=> setCount((counter) => counter - 1)}>-</button>
         <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + step)}>+</button>
+        <button onClick={()=> setCount((counter) => counter + 1)}>+</button>
       </div>
-
-      <p>
-        <span>
-          {count === 0
-            ? "Today is "
-            : count > 0
-            ? `${count} days from today is `
-            : `${Math.abs(count)} days ago was `}
-        </span>
-        <span>{date.toDateString()}</span>
-      </p>
     </div>
   );
 }
