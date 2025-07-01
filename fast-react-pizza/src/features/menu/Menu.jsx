12a66836af61e77b0f396bcad6a8c11b18124524
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-01 22:57:18 +0100

    commit

--- fast-react-pizza/src/features/menu/Menu.jsx
+++ fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,9 +1,11 @@
+import { getMenu } from "../../services/apiRestaurant";
 function Menu() {
   return <h1>Menu</h1>;
 }
 
-function loader(){
-  
+export async function Loader(){
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
