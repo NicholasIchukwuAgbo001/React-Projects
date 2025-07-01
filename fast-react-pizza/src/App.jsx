1d15e0fdc3b3fae9c10c4b9c7f9f8a88dc1af66f
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-02 00:28:32 +0100

    commit

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -9,27 +9,21 @@ import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
+    path: '/',
     element: <AppLayout />,
     errorElement: <Error />,
-
+    
     children: [
-      { 
-        path: '/', element: <Home /> 
-      },
+      { path: '', element: <Home /> },
       {
-        path: '/menu', 
-        element: <Menu />, loader: menuLoader,
+        path: 'menu',
+        element: <Menu />,
+        loader: menuLoader,
         errorElement: <Error />,
-       }, 
-      { 
-        path: '/cart', element: <Cart /> 
       },
-      { 
-        path: '/order/new', element: <CreateOrder /> 
-      },
-      { 
-        path: '/order/:orderId', element: <Order /> 
-      },
+      { path: 'cart', element: <Cart /> },
+      { path: 'order/new', element: <CreateOrder /> },
+      { path: 'order/:orderId', element: <Order /> },
     ],
   },
 ]);
