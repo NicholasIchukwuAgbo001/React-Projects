fddebfccc0ffa8aeeefd2d483bce284dc8e26d2c
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-01 09:27:12 +0100

    commit

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -26,6 +26,14 @@ function Form(){
     
     function handleSubmit(event){
         event.preventDefault();
+
+        if(!description) return;
+
+        const newItem = {description, quantity, packed: false, id: Date.now()};
+        console.log(newItem)
+        
+        setDescription("");
+        setQuantity(1);
     }
     return (
         <form className="add-form" onSubmit={handleSubmit}>
