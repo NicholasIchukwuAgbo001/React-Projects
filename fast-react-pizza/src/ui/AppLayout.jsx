2041a7807dab31737d7b54bd14aac2d24d650e0e
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-02 00:09:40 +0100

    commit

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,19 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
+import Loader from './Loader'; 
 
 function AppLayout() {
-    return (
-        <div>
-            <Header />
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
 
-            <main>
-                <Outlet />
-            </main>
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
 
-            <CartOverview />
-        </div>
-    )
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
