f7ef838cef4948d831d4dcb047941f270a85ae8c
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-02 01:09:15 +0100

    commit

--- fast-react-pizza/src/features/order/SearchOrder.jsx
+++ fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
 
     function HandleSubmit(e) {
         e.preventDefault();
+        if(!query) return;
+        navigate(`/order/${query}`);
+        setQuery("");
     }
 
   return (
