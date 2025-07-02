dfb2506d4db12cde6ec71598a44fc658b1081380
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-02 05:30:22 +0100

    COMMIT

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder"; 
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <AppLayout />,
     errorElement: <Error />,
-    
+
     children: [
       { path: '', element: <Home /> },
       {
@@ -21,9 +21,20 @@ const router = createBrowserRouter([
         loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: 'cart', element: <Cart /> },
-      { path: 'order/new', element: <CreateOrder /> },
-      { path: 'order/:orderId', element: <Order /> },
+      { 
+        path: 'cart', 
+        element: <Cart /> 
+      },
+      { 
+        path: 'order/new', 
+        element: <CreateOrder /> 
+      },
+      { 
+        path: 'order/:orderId', 
+        element: <Order />, 
+        loader: orderLoader,
+        errorElement: <Error /> 
+      },
     ],
   },
 ]);
