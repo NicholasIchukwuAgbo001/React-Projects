a90ff6158e4752b2dd04a2b713102187bbe1efcf
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-10 15:46:06 +0100

    commit

--- Redux/src/features/count/Counter.jsx
+++ Redux/src/features/count/Counter.jsx
@@ -1,4 +1,4 @@
-import { increment, decrement } from "./CounterSlice";
+import { increment, decrement, reset } from "./CounterSlice";
 import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
@@ -6,10 +6,30 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <button onClick={() => dispatch(decrement())}> - </button>
+    <div className="flex items-center gap-4 justify-center  ">
+      <button 
+      className="bg-red-300 px-4 py-1 rounded" 
+      onClick={() => dispatch(decrement())}
+      >
+      - 
+      </button>
+      
       <h1>Count: {count}</h1>
-      <button onClick={() => dispatch(increment())}> + </button>
+
+      <button
+       className="bg-green-500 px-4 py-1 rounded"
+       onClick={() => dispatch(increment())}
+       > 
+       + 
+       </button>
+      
+
+      <button
+        className="bg-red-500 px-4 py-1 rounded"
+        onClick={() => dispatch(reset())}
+      >
+        Reset
+      </button>
     </div>
   );
 };
