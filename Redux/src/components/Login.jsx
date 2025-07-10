4e15bec1d498767a710bc930c3c721914a0467f8
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-10 16:05:18 +0100

    commit

--- Redux/src/components/Login.jsx
+++ Redux/src/components/Login.jsx
@@ -3,17 +3,17 @@ const Login = () => {
     <form action="">
         <div>
             <label htmlFor="">First Name</label>
-            <input type="text" />
+            <input type="text" required />
         </div>
 
         <div>
             <label htmlFor="">Last Name</label>
-            <input type="text" />
+            <input type="text" required />
         </div>
 
         <div>
             <label htmlFor="">Password</label>
-            <input type="text" />
+            <input type="password" required />
         </div>
 
     </form>
--- Redux/src/components/Signup.jsx
+++ Redux/src/components/Signup.jsx
@@ -3,17 +3,17 @@ const Signup = () => {
     <form action="">
         <div>
             <label htmlFor="">First Name</label>
-            <input type="text" />
+            <input type="text" required />
         </div>
 
         <div>
             <label htmlFor="">Last Name</label>
-            <input type="text" />
+            <input type="text" required />
         </div>
 
         <div>
             <label htmlFor="">Password</label>
-            <input type="text" />
+            <input type="password" required/>
         </div>
 
     </form>
