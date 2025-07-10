9119a0c8ff9ca3cdc4d357ea678897b6f20e2d6a
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-10 15:05:59 +0100

    commit

--- Redux/src/features/count/Counter.jsx
+++ Redux/src/features/count/Counter.jsx
@@ -6,19 +6,17 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="flex items-center gap-6 justify-center p-6">
+    <div>
       <button
         onClick={() => dispatch(decrement())}
-        className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
       >
         -
       </button>
 
-      <h1 className="text-2xl font-bold">Count: {count}</h1>
+      <h1>Count: {count}</h1>
 
       <button
         onClick={() => dispatch(increment())}
-        className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
       >
         +
       </button>
