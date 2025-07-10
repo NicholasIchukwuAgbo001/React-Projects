c7f42c52ee50e80f09a6f4f21605dd4c9ba3c3b3
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-10 16:03:01 +0100

    commit

--- Redux/src/components/Signup.jsx
+++ Redux/src/components/Signup.jsx
@@ -1,9 +1,23 @@
 const Signup = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <form action="">
+        <div>
+            <label htmlFor="">First Name</label>
+            <input type="text" />
+        </div>
+
+        <div>
+            <label htmlFor="">Last Name</label>
+            <input type="text" />
+        </div>
+
+        <div>
+            <label htmlFor="">Password</label>
+            <input type="text" />
+        </div>
+
+    </form>
+ )
 }
 
 export default Signup
