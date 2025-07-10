bb3cb74a97128869420c9d9be18edbf1cbc211f1
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-10 16:42:15 +0100

    commit

--- Redux/src/features/count/CounterSlice.jsx
+++ Redux/src/features/count/CounterSlice.jsx
@@ -10,7 +10,9 @@ const counterSlice = createSlice({
       state.value += 1;
     },
     decrement: (state) => {
-      state.value -= 1;
+      if(state.value > 0) {
+        state.value -= 1;
+      }
     },
     reset: (state) => {
         state.value = 0;
